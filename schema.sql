f1d8aa699f09df937fa573114e585b7f5a4659d3
Muhammad Davlatov <umedjon02.ud@gmail.com>
2023-05-10 02:54:25 +0500

    Update table, added new values

--- schema.sql
+++ schema.sql
@@ -2,10 +2,11 @@
 
 CREATE TABLE animals (
   id INT GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR(250),
+  name VARCHAR(255),
   data_of_birth DATE,
   escape_attempts INT,
   neutered BOOLEAN,
   weight_kg DECIMAL(5,2),
+  species VARCHAR(255),
   PRIMARY KEY(id)
 );
