f17e2dbf5aef29970e46caefd867be827219abf0
Luis Zubia <soyzubia@gmail.com>
2023-05-15 16:16:01 -0700

    Apply Query Tuning to 'SELECT * FROM visits where vet_id = 2' to reduce its execution time

--- queries.sql
+++ queries.sql
@@ -161,3 +161,6 @@ LIMIT 1;
 EXPLAIN ANALYZE SELECT COUNT(*) FROM visits where animal_id = 4;
 EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
+
+-- Using query tuning technique, we can select vet_id only instead of '*', which reduces the execution time
+EXPLAIN ANALYZE SELECT vet_id FROM visits WHERE vet_id = 2;
